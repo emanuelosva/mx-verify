f04469cc9bd3e3d25462729f6ca78a8b0540dcf8
emanuelosva <emanuelosva@gmail.com>
2024-01-24 10:31:27 -0600

    :wrench: (seo) Add seo config

---
+++ config/index.ts
@@ -0,0 +1,18 @@
+export const MY_LINKS = {
+  github: "https://github.com/emanuelosva",
+}
+
+export const SITE = {
+  title: "MxVerify",
+  seo: {
+    title: "MxVerify - Verifica si un sitio web es oficial del gobierno de México",
+    description: "Evita caer en estafas, antes de realizar un pago o trámite, valida que lo haces desde un sitio oficial del Gobierno de México.",
+    authors: [
+      {
+        name: "Emanuel Osorio - emanuelosva",
+        url: MY_LINKS.github,
+      },
+    ],
+    keywords: ["gobierno", "méxico", "verificar", "validar", "seguridad", "pago", "tramite", "estafa", "phishing",],
+  },
+}
